05db480af893d1efc8ecdadf752290beb4484d79
GiottoLee <giottolee@outlook.com>
2019-05-12 20:00:51 +0800

    Add Service consummer using feign.

--- spring-cloud-demo-web-admin-feign/src/main/java/com/spring/cloud/web/admin/feign/controller/AdminController.java
+++ spring-cloud-demo-web-admin-feign/src/main/java/com/spring/cloud/web/admin/feign/controller/AdminController.java
@@ -1,6 +1,6 @@
-package com.spring.cloud.demo.web.admin.ribbon.controller;
+package com.spring.cloud.web.admin.feign.controller;
 
-import com.spring.cloud.demo.web.admin.ribbon.service.AdminService;
+import com.spring.cloud.web.admin.feign.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdminController {
-    @Autowired          //对类成员变量、方法及构造函数进行标注，完成自动装配的工作
+    @Autowired
     private AdminService adminService;
 
     @RequestMapping(value = "hi", method = RequestMethod.GET)
-    public String sayHi(String message){
+    public String sayHi(String message) {
         return adminService.sayHi(message);
     }
 }
---
+++ spring-cloud-demo-web-admin-feign/src/main/java/com/spring/cloud/web/admin/feign/service/AdminService.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.web.admin.feign.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "spring-cloud-demo-service-admin")
+public interface AdminService {
+
+    @RequestMapping(value = "hi", method = RequestMethod.GET)
+    public String sayHi(@RequestParam(value = "message") String message);
+}
